4fc43f1298e21eeb5578c5daf06ffb6d00edbcec
Tuhin Mallick <67542764+tuhinmallick@users.noreply.github.com>
2022-04-16 03:06:21 +0200

    Update __init__.py

--- src/apps/__init__.py
+++ src/apps/__init__.py
@@ -1,1 +1,3 @@
-from apps.contact_form import contact
+from apps.contact_form import contact as Contact
+from apps.twitter_sentiment.py import sentiment as Twitter
+from apps.reddit_sentiment.py import reedit_sentiments as Reddit
